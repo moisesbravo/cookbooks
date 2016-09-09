9cee572f805f9116756f09c222fb426aea0eb55b
Moises Bravo Castillo <mbravo@MacBook-Pro-de-Moises.local>
2016-09-09 00:43:43 -0300

    otros cambios

--- apachePhp/recipes/default.rb
+++ apachePhp/recipes/default.rb
@@ -6,6 +6,9 @@
 
 
 include_recipe "apache2"
+include_recipe "apache2::mod_rewrite"
+include_recipe "apache2::mod_deflate"
+include_recipe "apache2::mod_headers"
 include_recipe "iptables"
 include_recipe "php"
 include_recipe "php::module_mysql"
@@ -17,9 +20,25 @@ iptables_rule 'http' do
 end
 
 
+
+
 #Cambia el sitio por defecto de apache.
 apache_site "default" do
-  enable true
+  enable false
 end
 
 
+directory node["phpapp"]["path"] do
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
+  recursive true
+end
+
+#crea el el virutal host  para la aplicacion.
+web_app 'phpapp' do
+  template 'site.conf.erb'
+  docroot node['phpapp']['path']
+  server_name node['phpapp']['server_name']
+end
