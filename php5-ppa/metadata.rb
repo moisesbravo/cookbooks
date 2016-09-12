cf80ad242433fe49848ff9f474054cc4f3ba735c
Moises Bravo Castillo <mbravo@MacBook-Pro-de-Moises.local>
2016-09-12 18:03:12 -0500

    error

--- php5-ppa/metadata.rb
+++ php5-ppa/metadata.rb
@@ -3,7 +3,7 @@ maintainer       'alt3.io'
 license          'MIT'
 description      'Chef cookbook to install PHP 5.6 using Ondrej Sury PPA'
 version          '1.0.0'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
 
 depends "apt"
 supports "ubuntu"
