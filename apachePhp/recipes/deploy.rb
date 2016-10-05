0a878f9d84a894bfa99306fb46e6650063cdabc4
Moises Bravo Castillo <mbravo@MacBook-Pro-de-Moises.local>
2016-10-04 23:16:51 -0300

    se modifica el deploy

--- apachePhp/recipes/deploy.rb
+++ apachePhp/recipes/deploy.rb
@@ -1,18 +1,18 @@
-node[:deploy].each do |application, deploy|
-  if deploy[:application_type] != 'php'
-    Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
-    next
-  end
 
-  opsworks_deploy_dir do
-    user deploy[:user]
-    group deploy[:group]
-    path deploy[:deploy_to]
-    xChef::Log.debug(deploy[:deploy_to])
-  end
+app = search(:aws_opsworks_app).first
+app_path = default["apachePhp"]["path"]
 
-  opsworks_deploy do
-    deploy_data deploy
-    app application
-  end
+package "git" do
+  # workaround for:
+  # WARNING: The following packages cannot be authenticated!
+  # liberror-perl
+  # STDERR: E: There are problems and -y was used without --force-yes
+  options "--force-yes" if node["platform"] == "ubuntu" && node["platform_version"] == "14.04"
 end
+
+application app_path do
+  
+  git app_path do
+    repository app["app_source"]["url"]
+    revision app["app_source"]["revision"]
+  end
