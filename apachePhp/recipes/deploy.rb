31683e0df7436e41dcfa9548cde854f37b7cd6c8
Moises Bravo Castillo <mbravo@MacBook-Pro-de-Moises.local>
2016-10-04 23:49:04 -0300

    ota e

--- apachePhp/recipes/deploy.rb
+++ apachePhp/recipes/deploy.rb
@@ -10,11 +10,10 @@ package "git" do
   options "--force-yes" if node["platform"] == "ubuntu" && node["platform_version"] == "14.04"
 end
 
-application app_path do
-  
+
   git app_path do
     repository app["app_source"]["url"]
     revision app["app_source"]["revision"]
+     action :sync
   end
 
-  end 
