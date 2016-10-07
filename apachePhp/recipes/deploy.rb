15213785e43abe4e0e951ebe319ecab6d27453ee
Moises Bravo Castillo <mbravo@MacBook-Pro-de-Moises.local>
2016-10-07 01:13:12 -0300

    kl

--- apachePhp/recipes/deploy.rb
+++ apachePhp/recipes/deploy.rb
@@ -1,6 +1,8 @@
 
 app = search(:aws_opsworks_app).first
-app_path = node["apachePhp"]["deploy"]
+app_path = node["apachePhp"]["path"]
+app_proyecto = node["apachePhp"]["proyecto"]
+app_deploy =node["apachePhp"]["deploy"]
 
 package "git" do
   # workaround for:
@@ -11,7 +13,7 @@ package "git" do
 end
 
 
-  git app_path do
+  git app_deploy do
     repository app["app_source"]["url"]
     revision app["app_source"]["revision"]
      action :sync
@@ -19,8 +21,8 @@ end
 
 bash "install_deploy" do
   code <<-EOL
-  mv node["apachePhp"]["proyecto"] node["apachePhp"]["path"]
-  rm -r  node["apachePhp"]["deploy"]
+  mv   #{app_proyecto}/*  #{app_path} 
+  rm -r  #{app_deploy}
 
   EOL
 end
