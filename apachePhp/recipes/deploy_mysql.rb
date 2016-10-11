3758518c565f9d9eda725644a47d48d1a6d2aa09
Moises Bravo Castillo <mbravo@MacBook-Pro-de-Moises.local>
2016-10-10 23:40:25 -0300

    se agrega actualizacion de cliente

--- apachePhp/recipes/deploy_mysql.rb
+++ apachePhp/recipes/deploy_mysql.rb
@@ -1,3 +1,10 @@
+#actualiza Cliente
+execute "update-upgrade" do
+  command "apt-get update && apt-get upgrade -y"
+  action :run
+end
+
+
 # Instala Cliente Mysql 
 mysql_client 'default' do
   action :create
